eeaff8dec367345065e6f7d2856e2ba1781b5b5a
Administrator <Administrator@CHETUIWK1764>
2022-04-11 18:27:34 +0530

    Add project files.

---
+++ Crud_databas/Controllers/studentController.cs
@@ -0,0 +1,21 @@
+﻿using Crud_databas.DB_Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_databas.Controllers
+{
+    public class studentController : Controller
+    {
+        public IActionResult info()
+        {
+            
+            gurmeet10Context obj = new gurmeet10Context();
+            var re = obj.Logins.ToList();
+
+            return View(re);
+        }
+    }
+}
---
+++ Crud_databas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Crud_databas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
